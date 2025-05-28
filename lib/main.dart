7209fc1db91f0240b1228febbbb8b9f484ae13a1
TaengGyul <mjcho0130@gmail.com>
2025-05-28 14:27:45 +0900

    사진넣기 Container

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Constraints Example')),
-        body: ListView(
-          children: [
-            AspectRatio(
-              aspectRatio: 1 / 1,
-              child: Image.asset(
-                "assets/kurimanju.png",
-                fit: BoxFit.cover,
-              ),
-            ),
-          ],
+        body: Container(
+          color: Colors.red,
+          width: 300,
+          height: 300,
+          child: Image.asset(
+            "assets/kurimanju.png",
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
